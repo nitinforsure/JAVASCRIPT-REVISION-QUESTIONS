edbb25012054d42eaeed824da91a11901357c1ea
nitin swarnkar <5956nitin@gmail.com>
2025-06-04 23:33:41 +0530

    i saved this code to git

---
+++ Q2.js
@@ -0,0 +1,9 @@
+function reversestring(str){
+    let reversed = "";
+    for(let i = str.length-1; i>=0; i--){
+        reversed = reversed + str[i];
+    }
+    return reversed;
+}
+let input = "aanchal";
+console.log(reversestring(input));
